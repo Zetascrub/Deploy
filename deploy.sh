aaa680f630f66e9ebbf91c05e3b09c75afa77ff4
zetascrub <thomas.e.odonnell@gmail.com>
2021-07-13 05:12:13 -0400

    first commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# Packages
+
+## updates
+apt update
+apt -y upgrade
+apt -y dist-upgrade
+
+apt install -y terminator
+
+# Cosmetics
+
+## Background
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+    var allDesktops = desktops();
+    print (allDesktops);
+    for (i=0;i<allDesktops.length;i++) {{
+        d = allDesktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper",
+                                     "org.kde.image",
+                                     "General");
+        d.writeConfig("Image", "https://images8.alphacoders.com/746/746199.jpg")
+    }}
